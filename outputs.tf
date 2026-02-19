fbacdc4eb2e6c82740dd736d2546a8528871746d
DrInTech <samuelokesanya12@gmail.com>
2026-02-19 22:57:11 +0100

    fix: switch to local_file provider for apply demo

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,9 @@
 output "cluster_name" {
-  description = "The name of the created kind cluster"
-  value       = kind_cluster.demo.name
+  description = "The name of the cluster configuration"
+  value       = var.cluster_name
 }
 
-output "kubeconfig" {
-  description = "Kubeconfig for the created cluster"
-  value       = kind_cluster.demo.kubeconfig
-  sensitive   = true
+output "config_file_path" {
+  description = "Path to the generated cluster config file"
+  value       = local_file.cluster_config.filename
 }
